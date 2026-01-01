701836fd3db933847d3040373bc6fa1401f611ea
Yukta Baid <166050350+yukta2505@users.noreply.github.com>
2026-01-02 01:02:18 +0530

    Change CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-gt05.onrender.com",
     credentials: true
 }))
 
